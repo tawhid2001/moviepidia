6ded2ec3ff49c3c07fb7cc70cb0529341661936f
Tawhid Talal <tawhid.talal@gmail.com>
2026-04-23 20:40:30 -0400

    added toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState, useEffect } from 'react'
-import Navbar from './components/Navbar/Navbar.jsx'
-import Home from './pages/Home/Home.jsx'
-import SearchResults from './pages/SearchResults/SearchResults.jsx'
-import MovieDetails from './pages/MovieDetails/MovieDetails.jsx'
-import Auth from './pages/Auth/Auth.jsx'
-import Footer from './components/Footer/Footer.jsx'
-import { Routes, Route } from 'react-router-dom'
-import {onAuthStateChanged} from 'firebase/auth'
-import { Navigate } from 'react-router-dom'
-import { auth } from './firebase.js'
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Home from "./pages/Home/Home.jsx";
+import SearchResults from "./pages/SearchResults/SearchResults.jsx";
+import MovieDetails from "./pages/MovieDetails/MovieDetails.jsx";
+import Auth from "./pages/Auth/Auth.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import { Routes, Route } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { Navigate } from "react-router-dom";
+import { auth } from "./firebase.js";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-
   const [user, setUser] = useState(null);
   const [authLoaded, setAuthLoaded] = useState(false);
 
@@ -23,29 +24,39 @@ function App() {
   };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth,(user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
       setAuthLoaded(true);
     });
     return unsubscribe;
-  }, [])
+  }, []);
 
   return (
     <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        theme="dark"
+        toastClassName="custom-toast"
+        progressClassName="custom-progress"
+      />
       <Navbar user={user} />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/search' element={
-          <ProtectedRoute user={user}>
-            <SearchResults />
-          </ProtectedRoute>
-        } />
-        <Route path='/movie/:id' element={<MovieDetails />} />
-        <Route path='/auth' element={<Auth />} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/search"
+          element={
+            <ProtectedRoute user={user}>
+              <SearchResults />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/services/auth.js
+++ src/services/auth.js
@@ -5,18 +5,31 @@ import {
 } from "firebase/auth";
 import { auth } from "../firebase";
 import { createUserDocument } from "./users";
+import { toast } from "react-toastify";
 
 export const signup = async (email, name, password) => {
-  const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-  await createUserDocument({ ...userCredential.user, displayName: name });
-  return userCredential;
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    await createUserDocument({ ...userCredential.user, displayName: name });
+    toast.success("Account created successfully!");
+    return userCredential;
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code.split("/")[1].replaceAll("-", " "));
+  }
 };
 
 export const login = async (email, password) => {
   try {
     await signInWithEmailAndPassword(auth, email, password);
+    toast.success("Login successful!");
   } catch (error) {
     console.error(error);
+    toast.error(error.code.split("/")[1].replaceAll("-", " "));
   }
 };
 
